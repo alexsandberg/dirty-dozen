34b5043330b6fd1b256217a259fbc75bfa0dbc0c
Alex Sandberg <alexsandberg7@gmail.com>
2019-08-22 15:25:14 -0600

    Move all scripts to main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,8 +6,38 @@ function getInput() {
   getData(state, year);
 }
 
+function getData(state, year) {
+  let results = [];
+  let subResults = [];
+  let dirtyDoz = [];
+  
+  $.getJSON( "data/csvjson.json", function( json ) {
+    results = json.map(a => ({...a}));
 
-// function appendText() {
+      results.forEach(obj => {
+      if(obj.YEAR === year && obj.STATE === state) {
+        subResults.push(obj);
+      }
+    })
+
+      subResults.sort(function(a,b) {
+      return b.CO2E_EMISSION - a.CO2E_EMISSION;
+    });
+
+    dirtyDoz = subResults.slice(0,12);
+
+    let dirtyNames = [];
+
+    dirtyDoz.forEach(function(obj) {
+      dirtyNames.push(obj.FACILITY_NAME)
+    })
+
+    console.log( dirtyNames );
+  });
+}
+
+
+// function appendText(item) {
 //   let h2 = `<h2>Text.</h2>`;               // Create element with HTML  
 //   let txt2 = $("<p></p>").text("Text.");   // Create with jQuery
 //   let txt3 = document.createElement("p");  // Create with DOM
