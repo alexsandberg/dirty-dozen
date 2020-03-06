36cf7b2af1014675fc18512259fec37aa55bf37e
Alex Sandberg <alexsandberg7@gmail.com>
2020-03-06 16:36:05 -0700

    display markers for each facility on map

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,12 +3,28 @@
 let stateGeo = resultsDataDiv.getAttribute("data-state-geo");
 let stateLatLng = stateGeo.replace(/[(),]/g, "").split(" ");
 
+let facilityDataset = Array.from(document.getElementsByClassName("facilityDataDiv"));
+let facilityGeos = facilityDataset.map((facility) => facility.dataset.geo.split(","));
+facilityGeos.forEach(arr => arr.forEach(i => i.trim()));
+
 
 // google maps init
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: parseInt(stateLatLng[0]), lng: parseInt(stateLatLng[1]) },
+        center: { lat: parseFloat(stateLatLng[0]), lng: parseFloat(stateLatLng[1]) },
         zoom: 6
     });
+
+    for (let i = 0; i < facilityGeos.length; i++) {
+        let marker = new google.maps.Marker({
+            position: { lat: parseFloat(facilityGeos[i][0]), lng: parseFloat(facilityGeos[i][1]) },
+            map: map,
+            title: 'Hello World!'
+        });
+    }
+
 }
+
+
+
